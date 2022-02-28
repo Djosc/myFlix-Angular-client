bc93c2f2acb759aec06ff065cf1fa7a1f3adc1a4
Djosc <davidjoscaldwell@gmail.com>
2022-02-28 18:11:30 -0500

    Add user login form and modal

---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+// Used to close dialog on success
+import { MatDialogRef } from '@angular/material/dialog';
+
+// For using API calls
+import { UserRegistrationService } from '../fetch-api-data.service';
+
+// Used to display notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss'],
+})
+export class UserLoginFormComponent implements OnInit {
+  @Input() userData = { Username: '', Password: '' };
+
+  constructor(
+    public fetchApiData: UserRegistrationService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar
+  ) {}
+
+  ngOnInit(): void {}
+
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe(
+      (response) => {
+        this.dialogRef.close();
+        console.log(response);
+        this.snackBar.open(response, 'OK', {
+          duration: 2000,
+        });
+        // Add token and username to local storage variables
+        // ! might add more than just username to local storage to improve upon the react version
+        localStorage.setItem('user', response.user.Username);
+        localStorage.setItem('token', response.token);
+      },
+      (response) => {
+        // console.log(response);
+        this.snackBar.open(response, 'OK', {
+          duration: 2000,
+        });
+      }
+    );
+  }
+}

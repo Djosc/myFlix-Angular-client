d54792229c612ffc9bd6d5f77e2b8c95edb4901c
Djosc <davidjoscaldwell@gmail.com>
2022-03-01 09:11:59 -0500

    Add movie card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
@@ -16,13 +17,19 @@ export class AppComponent {
   // This will open the dialog when the signup button is clicked
   openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
-      width: '400px',
+      width: '300px',
     });
   }
 
   openLoginDialog(): void {
     this.dialog.open(UserLoginFormComponent, {
-      width: '400px',
+      width: '300px',
+    });
+  }
+
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px',
     });
   }
 }
